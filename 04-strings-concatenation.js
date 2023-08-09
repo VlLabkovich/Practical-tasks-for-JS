605dd131d9075ef2eb046931baa23471b8bd6a53
Vladislav <labkovich.vl@gmail.com>
2023-08-09 17:21:34 +0300

    feat: add strings concatenation

--- 04-strings-concatenation.js
+++ 04-strings-concatenation.js
@@ -10,3 +10,12 @@
  *
  * 3. Выведите значение последней переменной в консоль
  */
+
+const myName = 'Vlad';
+const mySecondName = 'Labkovich';
+let myProf = 'engineer';
+let variable = 'My name is ' + myName + ' ' + mySecondName + ' and I am ' + myProf;
+console.log(variable); // My name is Vlad Labkovich and I am engineer
+
+let variableTwo = `My name is ${myName} ${mySecondName} and I am ${myProf}`;
+console.log(variableTwo); // My name is Vlad Labkovich and I am engineer`
