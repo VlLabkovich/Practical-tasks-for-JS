eca1c4ce00368ae94271b62c6f985d37b618a6b4
Vladislav <labkovich.vl@gmail.com>
2023-08-09 17:35:29 +0300

    feat: add data types

--- 06-data-types.js
+++ 06-data-types.js
@@ -12,3 +12,18 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+const myString = 'Hello';
+const myNumber = 1;
+const myBolean  = true;
+const myNull = null;
+let myUndefined;
+const myObject = {};
+const myArray = [];
+console.log(typeof(myString)); // string
+console.log(typeof(myNumber)); // number
+console.log(typeof(myBolean)); // boolean
+console.log(typeof(myNull)); // object
+console.log(typeof(myUndefined)); // undefined
+console.log(typeof(myObject)); // object
+console.log(typeof(myArray)); // object
