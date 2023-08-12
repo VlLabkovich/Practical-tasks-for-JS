3fe5ce16cd92c03047a5ba1cb8549ddf812b74a3
Vladislav <labkovich.vl@gmail.com>
2023-08-12 22:41:56 +0300

    feat: add create object

--- 12-create-object.js
+++ 12-create-object.js
@@ -8,3 +8,12 @@
  * 2. Выведите в консоль строку
  * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
  */
+
+const myInfo = {
+    Name: 'Vladislav',
+    surname: 'Labkovich',
+    favoriteNumber: 7
+};
+
+console.log(`My name is ${myInfo.Name} ${myInfo.surname}
+ and my favorite number is ${myInfo.favoriteNumber}`); // My name is Vladislav Labkovich and my favorite number is 7
