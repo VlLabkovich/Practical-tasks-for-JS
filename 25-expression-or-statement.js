4d1c6bc50e72ea72a7aee14351b3f7e8fd2d9097
Vladislav <labkovich.vl@gmail.com>
2023-08-18 14:30:13 +0300

    feat: add expression or statement

--- 25-expression-or-statement.js
+++ 25-expression-or-statement.js
@@ -5,3 +5,41 @@
  *  - инструкция (statement)
  *  - выражение-инструкция (expression statement)
  */
+
+15; // expression statement
+
+// statement
+const myObject = { // expression
+  x: 10, 
+  y: true,
+} (expression);
+
+// expression statement
+myObject.z = 'abc'; // expression
+
+// expression statement
+delete myObject.x;
+
+
+// statement
+let newVariable;
+
+// expression statement
+newVariable = 30 + 5; // expression
+
+// exrpession statement
+console.log(newVariable);  
+                 ^
+             expression
+
+// statement
+if (newVariable > 10) {
+            ^
+        expression
+// exrpession statement
+  console.log(`${newVariable} больше 10`);
+              __________________________
+                          ^
+                      expression
+
+};
