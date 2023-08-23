e9d3452730f0f3f81f4ab4e3fbc938df618f8481
Vladislav <labkovich.vl@gmail.com>
2023-08-23 08:49:14 +0300

    feat: add variable let

--- 28-let.js
+++ 28-let.js
@@ -12,3 +12,11 @@ const myFavoriteAnimal = 'Cat'
 
 console.log(myFavoriteAnimal)
 // 'Cat'
+
+let myFavoriteAnimal = 'Monkey'
+
+console.log(myFavoriteAnimal) // 'Monkey'
+
+myFavoriteAnimal = 'Cat'
+
+console.log(myFavoriteAnimal) // 'Cat'
