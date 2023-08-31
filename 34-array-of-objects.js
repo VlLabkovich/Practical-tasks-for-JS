dbfff31d501fd736f5eac5ef47f3327e62255804
Vladislav <labkovich.vl@gmail.com>
2023-08-31 15:29:48 +0300

    feat: add object to array

--- 34-array-of-objects.js
+++ 34-array-of-objects.js
@@ -11,3 +11,15 @@
  *
  * 4. Выведите результирующий массив в консоль
  */
+
+const cars = [
+    {carBrand: 'BMW', price: 30, isAvailableForSale: true},
+    {carBrand: 'Hodna', price: 20, isAvailableForSale: true},
+    {carBrand: 'Mazda', price: 10, isAvailableForSale: false}
+];
+
+cars.push({carBrand: 'Lada', price: 7, isAvailableForSale: true});
+console.log(cars); // {carBrand: 'BMW', price: 30, isAvailableForSale: true}
+                   // {carBrand: 'Hodna', price: 20, isAvailableForSale: true} 
+                   // {carBrand: 'Mazda', price: 10, isAvailableForSale: false}
+                   // {carBrand: 'Lada', price: 7, isAvailableForSale: true}
